8f17e3663cd18ec2ebfe447f7522d3a2469242f7
Jared Malone <jaredmm@uw.edu>
2018-10-23 12:00:33 -0700

    changed comments in app.js

--- app.js
+++ app.js
@@ -1,18 +1,23 @@
-// imports
+/**
+ * This script configures the Express application. To manage API resources,
+ * we only need to:
+ *  1) Import a router that points to a js file
+ *  2) Map an endpoint name to the router
+ */
+
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-// routers
+// import routers for each endpoint
 var indexRouter = require('./routes/index');
 var helloRouter = require('./routes/hello');
 
-// instantiate the express application
+// Declare and configure the express application
 var app = express();
 
-// view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
@@ -22,10 +27,11 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-// map routes to each router
+// Map endpoints to a router
 app.use('/', indexRouter);
 app.use('/hello', helloRouter);
 
+// Error handlers
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
