e7283c027afbe0e5abe78aaa2bfe6b97f5b3746f
Jared Malone <jaredmm@uw.edu>
2018-10-29 21:10:22 -0700

    heroku master branch push test

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.setHeader('Content-Type', 'text/plain');
-  res.send("Hello.");
+  res.send("Hello, World!");
 });
 
 module.exports = router;
