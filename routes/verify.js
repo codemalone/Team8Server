0cc8d4f9d0b626f4a5cd3f803ab6e0f604dd2085
Jared Malone <jaredmm@uw.edu>
2018-11-03 00:12:25 -0700

    complted move account logic to utilities

--- routes/verify.js
+++ routes/verify.js
@@ -2,39 +2,25 @@
 const express = require('express');
 
 //Create connection to Heroku Database
-let db = require('../utilities/utils').db;
-
-let getHash = require('../utilities/utils').getHash;
+let validateEmail = require('../utilities/account').validateEmail;
 
 var router = express.Router();
 
 router.get('/', (req, res) => {
-    let user = { email: req.query.email, vCode: req.query.code };
+    let email = req.query.email;
+    let code = req.query.code;
     let response = {title: "Account Verification"};
-
         
-    if(user.email && user.vCode) {
-        db.one('SELECT MemberID, Salt, Hash FROM Members NATURAL JOIN RegistrationHashes WHERE Email=$1', [user.email])
-        .then(data => {
-            // if the hash values do not match then throw an exception
-            if (data.hash != getHash(user.vCode, data.salt)) {
-                throw({ message: "registration hash codes do not match"});
-            } else {
-                user.memberid = data.memberid;
-                console.log("Verified");
-            }
-        }).then(() => {
-            return db.none("UPDATE Members SET verification=1 WHERE memberid=$1", [user.memberid]);
-        }).then(() => {
-            return db.none("DELETE FROM RegistrationHashes WHERE memberid=$1", [user.memberid]);
-        }).then(() => {
-            response.message = "Your email address has been confirmed. You may now use our app!";
+    if(email && code) {
+        validateEmail(email, code)                
+        .then(() => {
+            response.message = "Your email has been verified. You can now use our app!";
             res.render('index', response);
-        }).catch((err) => {
+    }).catch((err) => {
             //If anything happened, send generic error to user and print stacktrace to console
-            console.dir(err.message);
             response.message = "Unable to verify email address."
             res.render('index', response);
+            console.dir(err.message);
         })
     } else {
         response.message = "Invalid input.";
--- utilities/account.js
+++ utilities/account.js
@@ -11,10 +11,11 @@ const getHash = require('./utils.js').getHash;
 const crypto = require("crypto");
 
 /**
- * Checks the provided credentials and returns the user details.
- * @param {string} email 
- * @param {string} theirPw 
- * @return {user} 
+ * Verifies the correct credentials and returns a user object.
+ * @param {string} email a registered user email
+ * @param {string} theirPw the associated user password
+ * @return {user} an object with all user profile data
+ * @throws "invalid credentials"
  */
 function getUser(email, theirPw) {
     //get the user data
@@ -27,7 +28,7 @@ function getUser(email, theirPw) {
             //Combined their password with our salt, then hash
             let theirSaltedHash = getHash(theirPw, salt); 
 
-        // If provided password is valid then return the user data
+        // If provided password is valid then finally return the user data
         if (ourSaltedHash == theirSaltedHash) {
             return data;
         } else {
@@ -36,28 +37,46 @@ function getUser(email, theirPw) {
     });
 }
 
-
-
 /**
- * Sends a validation email to the specified email address.
- * @param {} email address of an unvalidated user.
+ * Checks if an email address is registered and not validated. If the
+ * conditions hold then a validation link is sent to the address.
+ * 
+ * @param {} email address of a registered user.
  */
 function sendEmailValidationLink(email) {
+    let user; // store user data for the promise chain
     
-    // check that a user is registered and not already validated
+    return _getUserNoPassword(email)
+    .then(data => {
+        if (!data) {
+            throw("invalid credentials");
+        } else {
+            user = data;
+        }
+        
+        // check that a user has not already validated
+        if (user.verification == 1) {
+            throw("user email has been validated");
+        } 
 
+        // remove any existing validation codes
+        return _removeRegistrationCodes(user.memberid);    
+    }).then(() => {
+        // generate and store a new code
+        let vCode = crypto.randomBytes(8).toString("hex");
+        let vCodeHash = getHash(vCode, user.salt);
+        user.vCode = vCode;
 
-    // remove any existing validation codes
-
-
-    // generate and store a new code
-
-
-    // email a link to the user
-    let link = "http://tcss450a18-team8-test.herokuapp.com/verify?email=" + email + "&code=" + vCode;
-    let msg = "Welcome to our app! Please verify this email address by clicking the link below.<p>"
+        return db.none("INSERT INTO registrationhashes(hash, memberid) VALUES ($1, $2)",
+                [vCodeHash, user.memberid]);
+    }).then(() => {
+        // email a link to the user
+        let link = "http://tcss450a18-team8-test.herokuapp.com/verify?email=" + user.email + "&code=" + user.vCode;
+        let msg = "Welcome to our app! Please verify this email address by clicking the link below.<p>"
                 + "<a href=\"" + link + "\">" + link + "</a>";
-    sendEmail(email, "Verify your account", msg);
+        
+        sendEmail(user.email, "Verify your account", msg);
+    });
 }
 
 /**
@@ -66,12 +85,26 @@ function sendEmailValidationLink(email) {
  * @param {*} code provided in the email for validation
  */
 function validateEmail(email, code) {
+    let user; // store user data for the promise chain
+    
+    return _getUserAndValidationCode(email)
+    .then(data => {
+        if (!data) {
+            throw("invalid credentials");
+        } else {
+            user = data;
+        }
 
-
-
-
+        // if the hash values match then update user account
+        if (user.hash == getHash(code, user.salt)) {
+            return db.none("UPDATE Members SET verification=1 WHERE memberid=$1", [user.memberid]);    
+        } else {
+            throw("hash codes do not match");
+        }
+    }).then(() => {
+        return _removeRegistrationCodes(user.memberid);
+    })
 }
-
 
 /**
  * Changes a user password to the specified value.
@@ -93,7 +126,7 @@ function changePassword(email, oldPassword, newPassword) {
  * @param {*} email address of the user
  */
 function resetPassword(email) {
-    // lookup the user data
+    // lookup the user data and pass it to the next promise
     return _getUserNoPassword(email)
     .then(user => {
         // check if the user email has been validated.
@@ -115,6 +148,22 @@ function resetPassword(email) {
  */
 function _getUserNoPassword(email) {
     return db.one('SELECT * FROM Members WHERE Email=$1', [email]);
+}
+
+/**
+ * Helper function gets and returns a user with the validation hash.
+ * @param {string} email 
+ */
+function _getUserAndValidationCode(email) {
+    return db.one('SELECT * FROM Members NATURAL JOIN RegistrationHashes WHERE Email=$1', [email]);
+}
+
+/**
+ * Helper function to remove any existing registration hashes for the user.
+ * @param {int} memberid
+ */
+function _removeRegistrationCodes(memberid) {
+    return db.none("DELETE FROM registrationhashes WHERE memberid=$1", [memberid]);
 }
 
 /**
