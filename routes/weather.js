b551edcd286a7c483842b68351e5f9d59c6ad336
Jake Yang <jly0627@yahoo.com>
2018-11-02 21:01:16 -0700

    test

--- routes/weather.js
+++ routes/weather.js
@@ -54,9 +54,10 @@ function weathercall(lat, long, time, zip, res) {
             db.none("INSERT INTO WEATHER(zip, timestamp, hourlyweather, dailyweather) VALUES ($1, $2, $3, $4)", params);
             db.one("SELECT hourlyweather, dailyweather FROM WEATHER WHERE zip = $1", zip)
             .then(row => {
-                dailyweather = dailyweather.concat(hourlyweather);
+                body = Object.assign(dailyweather, hourlyweather);
+                //dailyweather = dailyweather.concat(hourlyweather);
                 //body = dailyweather;
-                res.send(dailyweather);
+                res.send(body);
             });
         }
     });
