876ffee36e68f350a5fedbac7d024e30069c5d2e
Jake Yang <jly0627@yahoo.com>
2018-11-02 20:00:55 -0700

    test

--- routes/weather.js
+++ routes/weather.js
@@ -29,7 +29,7 @@ router.post('/', (req, res) => {
         console.log(err);
         // (zip doesn't exist) = make weather call
         //weathercall(latitude, longitude, timestamp, zipcode, res);
-        res.send(weathercall(latitude, longitude, timestamp, zipcode, res));
+        weathercall(latitude, longitude, timestamp, zipcode, res);
     })
 });
 
@@ -44,7 +44,7 @@ function weathercall(lat, long, time, zip, res) {
             let dailyweather = { "test2": "hello2" };
             let params = [zip, time, hourlyweather, dailyweather];
             db.none("INSERT INTO WEATHER(zip, timestamp, hourlyweather, dailyweather) VALUES ($1, $2, $3, $4)", params);
-            res.write(body);
+            res.send(body);
         }
     });
 }
