c37ef84ab967f95fcfe96069ceb27267006eefe5
Jake Yang <jly0627@yahoo.com>
2018-11-02 18:23:20 -0700

    test

--- routes/weather.js
+++ routes/weather.js
@@ -13,18 +13,20 @@ router.post('/', (req, res) => {
     .then(row => {
         // (zip exists)
         // IF at least an hour difference = delete row + make weather call
+        console.log("zip exists");
         let parsed = row['timestamp'].split("/");
         if (parsed[0] != d.getFullYear() || parsed[1] != d.getMonth() || parsed[2] != d.getDate() || parsed[3] != d.getHours()) {
             db.none("DELETE FROM WEATHER WHERE zip = $1", zipcode);
             weathercall(latitude, longitude, timestamp, 5, res);
             // res.send({
-            //     zipcodeFound: changed
+            //     zipFound: changed
             // })
         }
         res.send({
-            zipcodeFound: notChanged
+            zipFound: notChanged
         })
     }).catch(() => {
+        console.log("zip doesnt exist");
         // (zip doesn't exist) = make weather call
         weathercall(latitude, longitude, timestamp, zipcode, res);
     })
