abfc07164c43f95a5fce10b455ff1cc36d8957a4
Jake Yang <jly0627@yahoo.com>
2018-11-02 20:14:02 -0700

    test

--- routes/weather.js
+++ routes/weather.js
@@ -10,36 +10,43 @@ router.post('/', (req, res) => {
     let longitude = 78.543;
     let timestamp = "" + d.getFullYear() + "/" + d.getMonth() + "/" + d.getDate() + "/" + d.getHours();
     db.one("SELECT * FROM WEATHER WHERE zip = $1", zipcode)
-    .then(row => {
-        // (zip exists)
-        // IF at least an hour difference = delete row + make weather call
-        let parsed = row['timestamp'].split("/");
-        if (parsed[0] != d.getFullYear() || parsed[1] != d.getMonth() || parsed[2] != d.getDate() || parsed[3] != (d.getHours() + 1)) {
-            db.none("DELETE FROM WEATHER WHERE zip = $1", zipcode);
-            console.log("DELETED + ADDING");
+        .then(row => {
+            // (zip exists)
+            // IF at least an hour difference = delete row + make weather call
+            let parsed = row['timestamp'].split("/");
+            if (parsed[0] != d.getFullYear() || parsed[1] != d.getMonth() || parsed[2] != d.getDate() || parsed[3] != (d.getHours())) {
+                db.none("DELETE FROM WEATHER WHERE zip = $1", zipcode);
+                console.log("DELETED + ADDING");
+                weathercall(latitude, longitude, timestamp, zipcode, res);
+            } else {
+                res.send({
+                    unchanged: true
+                })
+            }
+        }).catch((err) => {
+            console.log(err);
+            // (zip doesn't exist) = make weather call
             weathercall(latitude, longitude, timestamp, zipcode, res);
-        } else {
-            res.send({
-                unchanged: true
-            })
-        }
-    }).catch((err) => {
-        console.log(err);
-        // (zip doesn't exist) = make weather call
-        //weathercall(latitude, longitude, timestamp, zipcode, res);
-        weathercall(latitude, longitude, timestamp, zipcode, res);
-    })
+        })
 });
 
 function weathercall(lat, long, time, zip, res) {
-    let url = `https://api.weatherbit.io/v2.0/forecast/hourly?city=Raleigh,NC&key=${API_KEY}&hours=48`;
+    let url = `https://api.weatherbit.io/v2.0/forecast/daily?city=Raleigh,NC&key=${API_KEY}&days=10`;
+    request(url, function (error, response, body) {
+        if (error) {
+            res.send(error);
+        } else {
+            let dailyweather = { "test2": "hello2" };
+            res.send(body);
+        }
+    });
+    url = `https://api.weatherbit.io/v2.0/forecast/hourly?city=Raleigh,NC&key=${API_KEY}&hours=24`;
     request(url, function (error, response, body) {
         if (error) {
             res.send(error);
         } else {
             // res.send(response);
-            let hourlyweather = { "test": "hello" };
-            let dailyweather = { "test2": "hello2" };
+            let hourlyweather = body;
             let params = [zip, time, hourlyweather, dailyweather];
             db.none("INSERT INTO WEATHER(zip, timestamp, hourlyweather, dailyweather) VALUES ($1, $2, $3, $4)", params);
             res.send(body);
