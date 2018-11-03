f4a39baa4c8b9282bf39ca66875abf63687e1d8b
Jake Yang <jly0627@yahoo.com>
2018-11-03 16:49:54 -0700

    test

--- routes/weather.js
+++ routes/weather.js
@@ -7,43 +7,30 @@ var router = express.Router();
 var d = new Date();
 router.use(bodyParser.json());
 router.post('/', (req, res) => {
-    //let zipcode = req.body['zipcode'];
-    //let latitude = req.body['latitude'];
-    //let longitude = req.body['longititude'];
-    let zipcode = 255;
-    let latitude = 47.3223;
-    let longitude = -122.3126;
+    let zipcode = req.body['zipcode'];
+    let latitude = req.body['latitude'];
+    let longitude = req.body['longititude'];
+    //let zipcode = 255;
+    //let latitude = 47.3223;
+    //let longitude = -122.3126;
     let url = `https://api.weatherbit.io/v2.0/forecast/daily?lat=` + latitude + `&lon=` + longitude + `&key=${API_KEY}&days=1`;
-    let dailyweather = {};
     request(url, function (error, response, body) {
         if (error) {
             res.send(error);
         } else {
-            dailyweather = body;
+            let dailyweather = body;
             url = `https://api.weatherbit.io/v2.0/forecast/hourly?lat=` + latitude + `&lon=` + longitude + `&key=${API_KEY}&hours=3`;
             request(url, function (error, response, body) {
                 if (error) {
                     res.send(error);
                 } else {
                     let hourlyweather = body;
-                    //let zipJSON = { 'zip': zipcode }
                     body = [JSON.parse(dailyweather), JSON.parse(hourlyweather), {'zip': zipcode}];
                     res.send(body);
                 }
             });
         }
     });
-    // url = `https://api.weatherbit.io/v2.0/forecast/hourly?lat=` + latitude + `&lon=` + longitude + `&key=${API_KEY}&hours=3`;
-    // request(url, function (error, response, body) {
-    //     if (error) {
-    //         res.send(error);
-    //     } else {
-    //         let hourlyweather = body;
-    //         //let zipJSON = { 'zip': zipcode }
-    //         body = [dailyweather, hourlyweather, {'zip': zipcode}];
-    //         res.send(body);
-    //     }
-    // });
 });
 
 module.exports = router;
