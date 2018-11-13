eb3c67bbfda72b23df037bf40e4015429cbf479e
Jared Malone <jaredmm@uw.edu>
2018-11-12 20:59:24 -0800

    moved starter chat logic to chat_service.js

--- routes/chats.js
+++ routes/chats.js
@@ -8,7 +8,7 @@ const bodyParser = require("body-parser");
 const chat = require('../utilities/chat_service');
 
 // for now we can put the fcm module here
-let fcm_functions = require('../utilities/utils').fcm_functions;
+
 let db = require('../utilities/utils').db;
 
 // create the router
@@ -18,79 +18,32 @@ router.use(bodyParser.json());
 /**
  * Get all messages from any conversation between the caller and another user.
  */
-router.post('/message/get', (req, res) => {
+router.post('/message/getAll', (req, res) => {
     let token = req.body.token;
-    let otherUser = req.body.otherUser;
+    let chatId = req.body.chatId;
 
-    chat.doSomethingCool(token, otherUser)
+    chat.getAllMessages(token, chatId)
         .then(user => { res.send({ success: true, user: user }) })
         .catch(err => { res.send({ success: false, message: err }) })
 });
 
+router.post('/message/send', (req, res) => {
+    let token = req.body.token;
+    let email = req.body.email;
+    let chatId = req.body.chatId;
+    let message = req.body.message;
 
-
-//send a message to all users "in" the chat session with chatId
-router.post("/message/send", (req, res) => {
-    let email = req.body['email'];
-    let message = req.body['message'];
-    let chatId = req.body['chatId'];
-    if(!email || !message || !chatId) {
-        res.send({
-            success: false,
-            error: "Username, message, or chatId not supplied"
-        });
-        return;
-    }
-    //add the message to the database
-    let insert = 'INSERT INTO Messages(ChatId, Message, MemberId) SELECT $1, $2, '
-                    + 'MemberId FROM Members WHERE email=$3';
-    db.none(insert, [chatId, message, email])
-    .then(() => {
-
-        //send a notification of this message to ALL members with registered tokens
-        db.manyOrNone('SELECT * FROM FCM_Token')
-        .then(rows => {
-            rows.forEach(element => {
-                fcm_functions.sendToIndividual(element['token'], message, email);
-            });
-            res.send({
-                success: true
-            });
-        }).catch(err => {
-            res.send({
-                success: false,
-                error: err,
-            });
-        })
-    }).catch((err) => {
-        res.send({
-            success: false,
-            error: err,
-        });
-    });
+    chat.sendMessage(token, email, chatId, message)
+        .then(() => { res.send({ success: true }) })
+        .catch(err => { res.send({ success: false, message: err }) })
 });
 
-//Get all of the messages from a chat session with id chatid
-router.post("/message/getAll", (req, res) => {
-    let chatId = req.body['chatId'];
+router.post('/message/tokenTest', (req, res) => {
+    let token = req.body.token;
 
-    let query = `SELECT Members.Email, Messages.Message, 
-                to_char(Messages.Timestamp AT TIME ZONE 'PDT', 'YYYY-MM-DD HH24:MI:SS.US' ) AS Timestamp
-                FROM Messages
-                INNER JOIN Members ON Messages.MemberId=Members.MemberId
-                WHERE ChatId=$1 
-                ORDER BY Timestamp DESC`
-    db.manyOrNone(query, [chatId])
-    .then((rows) => {
-        res.send({
-            messages: rows
-        })
-    }).catch((err) => {
-        res.send({
-            success: false,
-            error: err
-        })
-    });
+    chat.somethingThatNeedsUser(token)
+        .then(user => { res.send({ success: true, user: user }) })
+        .catch(err => { res.send({ success: false, message: err }) })
 });
 
 module.exports = router;
--- utilities/chat_service.js
+++ utilities/chat_service.js
@@ -1,132 +1,117 @@
 //Get the connection to Heroku Database
 let db = require('./utils.js').db;
 
-//Get the hasher
-const getHash = require('./utils.js').getHash;
+// firebase module
+let fcm_functions = require('../utilities/utils').fcm_functions;
 
 //Error codes returned on failure
 const error = require('./error_codes.js');
 
 /**
- * A service function can be called by an endpoint. Pass in a token and
- * whatever else is needed.
- * @param {string} token
- * @param {string} otherUser
+ * Get all messages for the given chatId.
+ * @param {*} token 
+ * @param {*} chatId 
  */
-function doSomethingCool(token, otherUser) {
+function getAllMessages(token, chatId) {
     // args must not be null
-    if (!(token && otherUser)) {
+    if (!(token && chatId)) {
         return _handleMissingInputError();
     }
 
     // start a chain of events that eventually returns something useful
     return _getUserOnToken(token)
-        .then(thisUser => {
-
-            //if a user not returned then the token is bogus
-            if (!thisUser) {
-                _handleSessionError(error.INVALID_TOKEN);
-            }
-
-            // token is good and we have the associated thisUser
-            // to do something with
-            
-            
-
-            // put a return statement here to end the chain, or to pass
-            // a value on to the next chain member
-            return _addNewContact(thisUser, otherUser)
-            
-        }).then(() => {
-            // when the call to _addNewContact completes it will return here
-            // so assume that it was a success!
-
-
-
-            return finalResultToEndpoint;
-        })
+        .then(() => _getAllMessages(chatId));
 }
 
 
+function somethingThatNeedsUser(token) {
+    if (!(token)) {
+        return _handleMissingInputError();
+    }
+
+    //check the token
+    return _getUserOnToken(token)
+        .then(user => {
+
+            //now we have a user object
+            console.log("Hello " + user.firstname);
+
+            return _stripUser(user);
+        });
+}
+
+function sendMessage(token, email, chatId, message) {
+    if (!(email, chatId, message)) {
+        // token not required yet because we are not passing it from app
+        return _handleMissingInputError();
+    }
+
+    // get user on email because that's all we have
+    return _getUserOnEmailNoPassword(email) 
+        .then(user => _addMessage(chatId, message, user.memberid))
+        .then(() => _sendGlobalMessage(email, message));
+}
+
 // Helper functions for synchronous repeated work
 
-function _isPassword(theUser, theirPassword) {
-    let salt = theUser.salt;
-    let ourSaltedHash = theUser.password; 
-
-    //Combined their password with our salt, then hash
-    let theirSaltedHash = getHash(theirPassword, salt); 
-
-    // If provided password is valid then return true
-    return ourSaltedHash == theirSaltedHash;
+/**
+ * returns a user object without any sensitive information
+ */ 
+function _stripUser(theUser) {
+    return {
+        first: theUser.firstname,
+        last: theUser.lastname,
+        username: theUser.username,
+        email: theUser.email
+    }
 }
 
 
 // Database queries
-
-function _addUser(first, last, username, email, password, salt) {
-    let params = [first, last, username, email, password, salt]
-    
-    return db.none("INSERT INTO MEMBERS(FirstName, LastName, Username, Email, Password, Salt)"
-            + "VALUES ($1, $2, $3, $4, $5, $6)", params)
+function _getUserOnToken(token) {
+    return db.one('SELECT * FROM Members NATURAL JOIN FCM_Token WHERE token=$1', [token])
         .catch(err => {
-            //custom error handler to determine if user already exists
-            if (err.code == "23505" && err.constraint == "members_email_key") {
-                _handleAccountError(error.EMAIL_ALREADY_EXISTS);
-            } else if (err.code == "23505" && err.constraint == "members_username_key") {
-                _handleAccountError(error.USERNAME_ALREADY_EXISTS);
+            if (err.code == 0) {
+                _handleSessionError(error.INVALID_TOKEN);
             } else {
                 _handleDbError(err);
             }
-        })
+        });            
 }
 
+function _getAllMessages(chatId) {
+    let query = `SELECT Members.Email, Messages.Message, 
+    to_char(Messages.Timestamp AT TIME ZONE 'PDT', 'YYYY-MM-DD HH24:MI:SS.US' ) AS Timestamp
+    FROM Messages
+    INNER JOIN Members ON Messages.MemberId=Members.MemberId
+    WHERE ChatId=$1 
+    ORDER BY Timestamp DESC`
+
+    return db.any(query, [chatId])
+        .catch(err => _handleDbError(err));
+}
+
+function _addMessage(chatId, message, memberId) {
+    let insert = 'INSERT INTO Messages(ChatId, Message, MemberId) VALUES($1, $2, $3)';
+                        
+    return db.none(insert, [chatId, message, memberId])
+        .catch(err => _handleDbError(err));
+}
+
+function _sendGlobalMessage(senderEmail, message) {
+    return db.manyOrNone('SELECT * FROM FCM_Token')
+        .then(rows => {
+            rows.forEach(element => {
+                fcm_functions.sendToIndividual(element['token'], message, senderEmail);
+            });
+            Promise.resolve();
+        })
+        .catch(err => _handleDbError(err));
+}
+
+// take this out after token passing is implemented in app
 function _getUserOnEmailNoPassword(email) {
     return db.oneOrNone('SELECT * FROM Members WHERE Email=$1', [email])
-        .catch(err => _handleDbError(err));
-}
-
-function _getUserOnUsernameNoPassword(username) {
-    return db.oneOrNone('SELECT * FROM Members WHERE Username=$1', [username])
-        .catch(err => _handleDbError(err));
-}
-
-function  _getUserAndResetCode(email) {
-    return db.oneOrNone('SELECT * FROM Members LEFT JOIN ResetCodes ON ' +
-            'Members.MemberID = ResetCodes.MemberID ' +
-            'WHERE Email=$1 AND current_timestamp - interval \'$2 minute\' < timestamp',
-            [email, ACCOUNT_RECOVERY_CODE_EXPIRATION])
-        .catch(err => _handleDbError(err));
-}
-
-function _getUserAndValidationCode(email) {
-    return db.oneOrNone('SELECT * FROM Members LEFT JOIN RegistrationHashes ON ' + 
-            'Members.memberid = RegistrationHashes.memberid ' +
-            'WHERE Email=$1 AND current_timestamp - interval \'$2 minute\' < timestamp',
-            [email, ACCOUNT_VERIFICATION_CODE_EXPIRATION])
-        .catch(err => _handleDbError(err));
-}
-
-function _removeRegistrationCodes(memberid) {
-    return db.none("DELETE FROM registrationhashes WHERE memberid=$1", [memberid])
-        .catch(err => _handleDbError(err));
-}
-
-function _removeResetCode(memberid) {
-    return db.none("DELETE FROM ResetCodes WHERE MemberID = $1", [memberid])
-        .catch(err => _handleDbError(err));
-}
-
-function _setUserIsVerified(memberid) {
-    return db.none("UPDATE Members SET verification=1 WHERE memberid=$1", [memberid])
-        .catch(err => _handleDbError(err));
-}
-
-function _setUserPassword(memberid, newPassword) {
-    let salt = crypto.randomBytes(32).toString("hex");
-    let saltedHash = getHash(newPassword, salt);
-
-    return db.none("UPDATE Members SET Password=$1, Salt=$2 WHERE Memberid=$3", [saltedHash, salt, memberid])
         .catch(err => _handleDbError(err));
 }
 
@@ -153,5 +138,5 @@ function _handleMissingInputError() {
 
 // any function included in exports will be public
 module.exports = {
-    doSomethingCool
+    getAllMessages, sendMessage, somethingThatNeedsUser
 }
